2521b0680986b9c9871a2d50a62989e516c9dde3
suzuki ten <stuzeukni0901@icloud.com>
2024-11-22 21:30:00 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Title",
+  description: "description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ja">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
